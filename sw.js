a71a931c768ee5f68f4e058f377ad6bc43a49108
Cuco9 <rogeralbertoperezguevara@gmail.com>
2026-06-03 19:09:23 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-const CACHE = 'voleytorneo-v1';
+const CACHE = 'voleytorneo-v2';
 const FILES = [
-  '/',
-  '/index.html',
-  '/css/styles.css',
-  '/js/data.js',
-  '/js/app.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
